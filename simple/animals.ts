7c3ef5eacad8ec90a748f1f0f776a5534a7bfcb1
Vaibhav Pachauri <Vaibhav Pachauri>
2017-03-26 00:05:30 +0530

    showing the result in the body

--- simple/animals.ts
+++ simple/animals.ts
@@ -1,26 +1,25 @@
 class Animal {
-    constructor(public name) { }
-    move(meters) {
-        console.log(this.name + " moved " + meters + "m.");
+    constructor(public name: string) { }
+    move(meters : number) : string{
+        return this.name + " moved " + meters + "m.";
     }
 }
 
 class Snake extends Animal {
-    move() {
+    move() : string{
         console.log("Slithering...");
-        super.move(5);
+        return super.move(5);
     }
 }
 
 class Horse extends Animal {
-    move() {
+    move() : string{
         console.log("Galloping...");
-        super.move(45);
+        return super.move(45);
     }
 }
 
-var sam = new Snake("Sammy the Python");
-var tom: Animal = new Horse("Tommy the Palomino");
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomino");
 
-sam.move();
-tom.move(23);
+document.body.innerHTML = sam.move();
